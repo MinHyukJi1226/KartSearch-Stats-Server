cdd38412452f03fc98d43442b2f433ff4016e1bd
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-03-29 11:44:56 +0900

    init

---
+++ kartsearch/src/main/kotlin/com/hcraestrak/kartsearch/KartsearchApplication.kt
@@ -0,0 +1,11 @@
+package com.hcraestrak.kartsearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KartsearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<KartsearchApplication>(*args)
+}
---
+++ kartsearch/src/test/kotlin/com/hcraestrak/kartsearch/KartsearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hcraestrak.kartsearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KartsearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
