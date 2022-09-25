8bc1ef9775cdafb4d7cb17982a50c50f9499c054
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-09-25 14:21:49 +0900

    [ADD] Project Create

---
+++ KartSearch/src/main/java/com/hcraestrak/kartsearch/KartSearchApplication.java
@@ -0,0 +1,13 @@
+package com.hcraestrak.kartsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KartSearchApplication.class, args);
+    }
+
+}
---
+++ KartSearch/src/test/java/com/hcraestrak/kartsearch/KartSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcraestrak.kartsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
